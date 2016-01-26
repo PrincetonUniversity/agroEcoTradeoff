eaf1b1b798f84908b3d779f73f996f76807aba80
ldemaz <lestes@princeton.edu>
2016-01-26 12:03:01 -0500

    Updated installer.R

--- installer.R
+++ installer.R
@@ -1,7 +1,6 @@
 # Execute these lines to install the library
-install.packages("devtools")
+install.packages("devtools", repos = 1:2)
 library(devtools)
+install_github("ldemaz/dtraster")
+install_github("PrincetonUniversity/lmisc")
 install_github("PrincetonUniversity/agroEcoTradeoff@devel")
-library(agroEcoTradeoff)
-
-
